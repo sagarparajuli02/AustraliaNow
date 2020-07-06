c33ba9d747189c752a1f6d9cd65744b11c01365b
Sagar Parajuli <official.sagar@yahoo.com>
2020-07-06 19:02:02 +1000

    Initial commit

---
+++ app/src/main/java/com/australia/now/MainActivity.java
@@ -0,0 +1,14 @@
+package com.australia.now;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
