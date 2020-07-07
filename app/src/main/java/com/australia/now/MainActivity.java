f246c71bc2db1815c155814f2d4a60a3148790bb
Sagar Parajuli <official.sagar@yahoo.com>
2020-07-07 19:18:05 +1000

    First commit

--- app/src/main/java/com/australia/now/MainActivity.java
+++ app/src/main/java/com/australia/now/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView,adBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setElevation(0);
+
+        // loading banner ad
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        adBottom=findViewById(R.id.adViewBottom);
+        AdRequest adRequestBottom = new AdRequest.Builder().build();
+        adBottom.loadAd(adRequestBottom);
     }
 }
